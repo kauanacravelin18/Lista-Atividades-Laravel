f99e05876d4eac75764e08e86d6275031d1801dd
Kauana Cravelin <engs-kauanacravelin@camporeal.edu.br>
2026-09-08 09:25:45 -0300

    ATV 2: Cria rotas com parâmetro /produto/{id}, /categoria/{id} e /usuario/{id}

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,15 @@ Route::get('/alunos', function () {
 Route::get('/contato', function () {
     return 'Esta é a página de Contato.';
 });
+
+Route::get('/produto/{id}', function ($id) {
+    return "Você está vendo o produto de ID: $id";
+});
+
+Route::get('/categoria/{id}', function ($id) {
+    return "Você está vendo a categoria de ID: $id";
+});
+
+Route::get('/usuario/{id}', function ($id) {
+    return "Você está vendo o usuário de ID: $id";
+});
