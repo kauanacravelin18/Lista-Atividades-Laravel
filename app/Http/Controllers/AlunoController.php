b66a17b218911b1263b4f51f60c8b72ca95d9e1b
Kauana Cravelin <engs-kauanacravelin@camporeal.edu.br>
2026-09-08 11:07:45 -0300

    ATV 13: implementa CRUD completo de Alunos no Controller e rotas resource

---
+++ app/Http/Controllers/AlunoController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Aluno;
+use Illuminate\Http\Request;
+
+class AlunoController extends Controller
+{
+    public function index()
+    {
+        $alunos = Aluno::all();
+        return view('alunos.index', compact('alunos'));
+    }
+
+    public function show($id)
+    {
+        $aluno = Aluno::findOrFail($id);
+        return view('alunos.show', compact('aluno'));
+    }
+
+    public function create()
+    {
+        return view('alunos.create');
+    }
+
+    public function store(Request $request)
+    {
+        Aluno::create($request->all());
+        return redirect()->route('alunos.index');
+    }
+
+    public function edit($id)
+    {
+        $aluno = Aluno::findOrFail($id);
+        return view('alunos.edit', compact('aluno'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $aluno = Aluno::findOrFail($id);
+        $aluno->update($request->all());
+        return redirect()->route('alunos.index');
+    }
+
+    public function destroy($id)
+    {
+        Aluno::destroy($id);
+        return redirect()->route('alunos.index');
+    }
+}
