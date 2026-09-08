7892438b07edcd84f76a7f458ed66a98723fa95d
Kauana Cravelin <engs-kauanacravelin@camporeal.edu.br>
2026-09-08 09:20:38 -0300

    ATV 1: Cria rotas /sobre, /alunos e /contato

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sobre', function () {
+    return 'Esta é a página Sobre.';
+});
+
+Route::get('/alunos', function () {
+    return 'Esta é a página de Alunos.';
+});
+
+Route::get('/contato', function () {
+    return 'Esta é a página de Contato.';
+});
