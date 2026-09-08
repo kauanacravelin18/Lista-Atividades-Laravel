8cb49944f19bda8e18dd67ea81fc898314d234b6
Kauana Cravelin <engs-kauanacravelin@camporeal.edu.br>
2026-09-08 10:10:59 -0300

    ATV 7: Cria layout compartilhado em layouts/app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title', 'Sistema de Alunos')</title>
+</head>
+<body>
+
+    <main>
+        @yield('content')
+    </main>
+
+</body>
+</html>
