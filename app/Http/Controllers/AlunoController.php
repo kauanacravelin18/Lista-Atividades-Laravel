088f3684df80e948f94f290f70b9006ab3207c23
Kauana Cravelin <engs-kauanacravelin@camporeal.edu.br>
2026-09-08 09:59:13 -0300

    ATV 4: Implementa os 7 métodos do CRUD no AlunoController

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -6,5 +6,39 @@ use Illuminate\Http\Request;
 
 class AlunoController extends Controller
 {
-    //
+  
+    public function index()
+    {
+        return 'Listando todos os alunos.';
+    }
+
+    public function create()
+    {
+        return 'Formulário para criar um novo aluno.';
+    }
+
+    public function store(Request $request)
+    {
+        return 'Salvando um novo aluno.';
+    }
+
+    public function show($id)
+    {
+        return "Mostrando detalhes do aluno de ID: $id";
+    }
+
+    public function edit($id)
+    {
+        return "Formulário para editar o aluno de ID: $id";
+    }
+
+    public function update(Request $request, $id)
+    {
+        return "Atualizando o aluno de ID: $id";
+    }
+
+    public function destroy($id)
+    {
+        return "Removendo o aluno de ID: $id";
+    }
 }
