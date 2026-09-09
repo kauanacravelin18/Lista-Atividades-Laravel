719aded4db3932206d6c0bd143e9f7439572de3b
Kauana Cravelin <engs-kauanacravelin@camporeal.edu.br>
2026-09-09 09:03:26 -0300

    ATV 21: cria middleware para controle de acesso

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/admin', function () {
+    return 'Área exclusiva do administrador';
+})->middleware('checkrole');
+
 require __DIR__.'/auth.php';
